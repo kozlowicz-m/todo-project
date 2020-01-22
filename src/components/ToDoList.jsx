279e9cb762149ad04f3d34c23e58c310cd8061fc
Marta Kozlowicz <Marta.Kozlowicz@stepstone.com>
2020-01-22 15:58:49 +0100

    Add filter, map

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -3,19 +3,35 @@ import React from "react";
 export default class ToDoList extends React.Component {
   constructor(props) {
     super(props);
+    this.input = React.createRef();
+    this.currentId = 0;
+
     this.state = {
-      item: "",
+      activeFilter: "active",
       list: []
     };
   }
 
-  //  this.setState( setState({
-  //       item: state.item
-  //     }));
-
-  handleSubmit(event) {
+  addItem = event => {
     event.preventDefault();
-  }
+
+    const { value } = this.input.current;
+
+    if (!value) return;
+
+    this.input.current.value = "";
+
+    this.setState({
+      list: [
+        ...this.setState.list,
+        {
+          value,
+          active: true,
+          id: this.currentId++
+        }
+      ]
+    });
+  };
 
   render() {
     return (
@@ -23,18 +39,44 @@ export default class ToDoList extends React.Component {
         <h1>Todo list</h1>
 
         <form onSubmit={this.handleSubmit}>
-          <input type="text" />
-          <button type="submit">Add</button>
+          <input type="text" ref={this.myRef} />
+          <button type="submit"> Add </button>
         </form>
 
         <ul>
-          <li>{this.setState.items}</li>
+          {this.state.list
+            .filer(item => {
+              switch (activeFilter) {
+                case "active":
+                  return cos;
+                  break;
+                case "done":
+                  return cos;
+                  break;
+                default:
+                  return true;
+              }
+            })
+            .map(item => (
+              <li key={cos} onClick={cos} className={cos}>
+                {cos}
+              </li>
+            ))}
         </ul>
 
         <div>
-          <button onClick={this.handleSubmit}>All</button>
-          <button onClick={this.handleSubmit}>Done</button>
-          <button onClick={this.handleSubmit}>Active</button>
+          <button onClick={this.cos} className="btn">
+            {" "}
+            All{" "}
+          </button>
+          <button onClick={this.cos} className="btn">
+            {" "}
+            Done{" "}
+          </button>
+          <button onClick={this.cos} className="btn">
+            {" "}
+            Active{" "}
+          </button>
         </div>
       </div>
     );
